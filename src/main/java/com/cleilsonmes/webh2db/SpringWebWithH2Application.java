c88139b2e1dfe365f6261c17ea068885c7b72c87
Cleilson <cleilsonmes@gmail.com>
2021-02-23 21:19:11 -0300

    Commit Initial

---
+++ src/main/java/com/cleilsonmes/webh2db/SpringWebWithH2Application.java
@@ -0,0 +1,13 @@
+package com.cleilsonmes.webh2db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebWithH2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebWithH2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/cleilsonmes/webh2db/SpringWebWithH2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cleilsonmes.webh2db;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebWithH2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
